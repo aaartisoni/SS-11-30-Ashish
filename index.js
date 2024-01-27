3967fe0b6b3d611641939e32070454c0eddf1121
Vikas Chaudhary DICE <vikas@dice-academy.com>
2024-01-27 12:57:18 +0530

    code pushed

---
+++ index.js
@@ -0,0 +1,42 @@
+
+// let oddsum=0;
+// let evensum=0;
+// for(let num=1;num<=100;num++){
+//     if(num%2==0){
+//         evensum=evensum+num;
+//     }else{
+//         oddsum=oddsum+num;
+//     }
+    
+// }
+// console.log(oddsum,evensum)
+
+
+let sum3=0;
+let sum5=0;
+let sum7=0;
+
+for(let num=1;num<=100;num++){
+    if(num%3==0){
+        sum3+=num;
+
+    }
+    else if(num%5==0){
+        sum5+=num;
+
+    }
+    else if(num%7==0){
+        sum7+=num;
+
+    }
+   
+
+
+
+
+}
+console.log(sum3,sum5,sum7)
+
+
+//  Sum of prime number bw 1 to 1000
+
